5c58f8b177a101ab5635ffc777ccb4da7af17e1d
shrutibhongal-web <shrutibhongal@gmail.com>
2026-03-14 23:10:06 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder="../templates")
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def home():
+    if request.method == "POST":
+        # form data process
+        return "Form submitted"
+
     return render_template("index.html")
 
 if __name__ == "__main__":
