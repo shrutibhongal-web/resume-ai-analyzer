cd0a49ba8db541061e8c03a9586b2fc3a4a30d22
shrutibhongal-web <shrutibhongal@gmail.com>
2026-03-14 23:16:17 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,16 +1,25 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__, template_folder="../templates")
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    ai_result = None
+
     if request.method == "POST":
+        file = request.files.get("resume")
 
-        name = request.form.get("name")
+        if file:
+            
+            filepath = os.path.join("uploads", file.filename)
+            os.makedirs("uploads", exist_ok=True)
+            file.save(filepath)
 
-        return render_template("result.html", name=name)
+            # Temporary result
+            ai_result = "Resume uploaded successfully! AI analysis will come here."
 
-    return render_template("index.html")
+    return render_template("index.html", ai_result=ai_result)
 
 if __name__ == "__main__":
     app.run()
